daf55f90955c3892b194618740b0db07c229bf40
ThibaultGH <thibault.cimic@inria.fr>
2022-10-12 00:13:13 +0200

    CMakeLists.txt modified so that CMake actually builds a libFileManagerCPP.a in out/build/.

---
+++ FileManagerCPP.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+#ifndef FILEMANAGERCPP_H
+#define FILEMANAGERCPP_H
+
+using namespace std;
+
+vector<string> split(const string str, const char separator);
+
+#endif
