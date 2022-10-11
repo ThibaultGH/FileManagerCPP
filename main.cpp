57e69bcc3e3689978928845d0795fb71eca3afdc
ThibaultGH <thibault.cimic@inria.fr>
2022-10-11 23:13:57 +0200

    Just some example of how vectors and string interacts in order to built a function that takes a string and split into several contained in a vector according to some separator.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-  cout << "Hello there !" << endl;
+
+  string full_str = "Here is my full string !";
+  vector<string> my_vec;// = {"Here","is","my","full","string","!"};
+
+  cout << "We're here !" << endl;
+
+  my_vec.push_back("Here");
+  my_vec.push_back("is");
+  my_vec.push_back("my");
+  my_vec.push_back("full");
+  my_vec.push_back("string");
+  my_vec.push_back("!");
+
+  
+  // my_vec[0] = "Here";
+  // my_vec[1] = "is";
+  // my_vec[2] = "my";
+  // my_vec[3] = "full";
+  // my_vec[4] = "string";
+  // my_vec[5] = "!";
+
+  cout << "My string before :" << endl << full_str << endl;
+  cout << "My string after :" << endl;
+  for (int i0 = 0; i0 < 6; ++i0) {
+    cout << my_vec[i0] << endl;
+  }
+
   
   return 0;
 }
